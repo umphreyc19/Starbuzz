4239004f3f198c067240ad6752ba9dad71c975c8
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2018-05-17 11:11:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/zx/starbuzz/DrinkActivity.java
@@ -0,0 +1,31 @@
+package com.example.zx.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class DrinkActivity extends Activity {
+
+    public static final String EXTRA_DRINKNO = "drinkNo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+        int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        photo.setImageResource(drink.getImageResourceId());
+        photo.setContentDescription(drink.getName());
+
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText(drink.getName());
+
+        TextView description = (TextView)findViewById(R.id.description);
+        description.setText(drink.getDescription());
+    }
+}
